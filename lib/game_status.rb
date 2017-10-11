41f052bab7c5e99136afff2bc230881418548150
gitluky <jluk87@gmail.com>
2017-10-11 17:34:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,9 +7,9 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won?(board)
   WIN_COMBINATIONS.each do |set|
-    board.set[0] == board.set[1] &&
-    board.set[1] == board.set[2] &&
-    position_taken?(board,set[0])
+    board[set[0]] == board[set[1]] &&
+    board[set[1]] == board[set[2]] &&
+    position_taken?(board,board[set[0]])
   end
 end
 
