745f8e2630d2268dfa6710ebb6f5212cc52b5ec5
gitluky <jluk87@gmail.com>
2017-10-11 17:33:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,21 +6,10 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won?(board)
-  count = 0
   WIN_COMBINATIONS.each do |set|
-    winning_combination = []
-    three_set = []
-    count+=1
-    set.each do |index|
-      winning_combination.push(index)
-      three_set.push(board[index])
-      if three_set == ["X","X","X"] || three_set == ["O","O","O"]
-        return winning_combination
-      end
-    end
-    if count == 8
-      return false
-    end
+    board.set[0] == board.set[1] &&
+    board.set[1] == board.set[2] &&
+    position_taken?(board,set[0])
   end
 end
 
